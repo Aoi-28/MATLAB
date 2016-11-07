be7c3605b300ca03371dbb25ca981f469684c6fc
Ishikawa Toshiki <14ec010@ms.dendai.ac.jp>
2016-11-07 12:04:59 +0900

    編集

--- Program/Program1.m
+++ Program/Program1.m
@@ -1,3 +1,9 @@
 IMG = imread('http://img01.gahag.net/201601/06o/gahag-0043319665.jpg');
-image(IMG);axis image;
-'PixelRegion';
+image(IMG);%axis image;
+pause;
+
+A = imresize(IMG,0.5);
+B = imresize(A,2,'box');
+image(A);
+pause;
+image(B)
