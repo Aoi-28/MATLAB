f9a40c111fe4bdea46cc6b2b46f1fcab8ae7cd8f
Ishikawa Toshiki <14ec010@ms.dendai.ac.jp>
2016-11-10 07:51:03 +0900

    ソースコード変更

--- Program/Program1.m
+++ Program/Program1.m
@@ -2,14 +2,17 @@ IMG = imread('http://photosku.com/images_file/small_images/s004_420.jpg');
 image(IMG);axis image;
 pause;
 
+%画像を1/2倍した後に2倍に単純拡大する。
 A = imresize(IMG,0.5);
 B = imresize(A,2,'box');
-imagesc(B);axis image;
+image(B);axis image;
 pause;
 
+%1/2倍画像を更に1/2倍(すなわち1/4倍)した後に4倍に単純拡大
+%以下同様に8倍16倍と2の階乗ずつ増やしていく。
 A = imresize(A,0.5);
 B = imresize(A,4,'box');
-imagesc(B);axis image;
+image(B);axis image;
 pause;
 
 A = imresize(A,0.5);
