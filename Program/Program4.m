9c26d257afe083d7c64966ff987f4902b53b5fe7
Aoi <14ec010@ms.dendai.ac.jp>
2016-11-25 16:27:01 +0900

    6章完成

--- Program/Program4.m
+++ Program/Program4.m
@@ -4,46 +4,16 @@
 % 課題作成にあたっては「Lenna」以外の画像を用いよ．
 
 IMG = imread('../Report/Report_Picture_04/Beget.jpg');
-image(IMG);
+imagesc(IMG);
 
 %グレースケール画像への変換
-Gray = rgb2gray(IMG);
+IMG = rgb2gray(IMG);
 colormap(gray);
 colorbar;
 pause;
-image(Gray);
+imagesc(IMG);
 
-imhist(Gray);
-
-%2階調画像
-IMG_2 = IMG > 256/2;
-imagesc(IMG_2);
-axis image;
+imhist(IMG);%濃度ヒストグラム生成
 pause;
 
-imhist(IMG_2);
 
-%4階調画像
-IMG_4_1 = IMG > 256/4;
-IMG_4_2 = IMG > 256/4 * 2;
-IMG_4_3 = IMG > 256/4 * 3;
-IMG_4 = IMG_4_1+IMG_4_2+IMG_4_3;
-imagesc(IMG_4);
-axis image
-pause;
-
-imhist(IMG_4);
-
-%8階調画像へ変更。これも4階調の時と同様で、8分割
-IMG_8_1 = IMG > 256/8;
-IMG_8_2 = IMG > 256/8 *2;
-IMG_8_3 = IMG > 256/8 *3;
-IMG_8_4 = IMG > 256/8 *4;
-IMG_8_5 = IMG > 256/8 *5;
-IMG_8_6 = IMG > 256/8 *6;
-IMG_8_7 = IMG > 256/8 *7;
-IMG_8 = IMG_8_1+IMG_8_2+IMG_8_3+IMG_8_4+IMG_8_5+IMG_8_6+IMG_8_7;
-imagesc(IMG_8);axis image;
-pause;
-
-imhist(IMG_8);
