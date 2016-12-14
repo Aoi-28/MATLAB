8e821d6c2367d8c26fd61e61703b09a4ea2bc9e9
aoi <14ec010@ms.dendai.ac.jp>
2016-12-14 23:17:31 +0900

    レポート5章

--- Program/Program5.m
+++ Program/Program5.m
@@ -4,15 +4,54 @@
 % 課題作成にあたっては「Lenna」以外の画像を用いよ．
 
 %モノクロ画像生成
-IMG = imread('../Report/Report_Picture_05/Baby.jpg');
+IMG = imread('../Report/Report_Picture_05/Mandrill.bmp');
 IMG = rgb2gray(IMG);
 colormap(gray);
 colorbar;
 imagesc(IMG);
 pause;
 
-%画素数
+%ヒストグラム生成
+hist = imhist(IMG);
 
-%平均
+%最大値、しきい値、全体平均の定義
+max = 0;
+threshold = 1;
+ave = mean(hist);
 
-%分散
+%ヒストグラムから黒くラスト白クラスに分割
+for i = 1:255
+    Cl1 = hist(1:i);
+    Cl2 = hist(i+1:256);
+
+    %画素数(sum関数)
+    omg1 = sum(Cl1);
+    omg2 = sum(Cl2);
+    %平均(mean関数)
+    ave1 = mean(Cl1);
+    ave2 = mean(Cl2);
+    %分散(var関数)
+    sig1 = var(Cl1);
+    sig2 = var(Cl2);
+    
+    %クラス内分散
+    clsigin = (omg1*sig1+omg2*sig2)/(omg1+omg2);
+    
+    %クラス間分散
+    clsigout = (omg1*(ave1-ave)^2+omg2*(ave2-ave)^2)/(omg1+omg2);
+
+    %クラス間分散/クラス内分散
+    separation = clsigout/clsigin;
+    
+    if max < separation
+        max = separation;
+        threshold = i;
+    end
+end
+
+%しきい値による2値処理
+IMG = IMG > threshold;
+imagesc(IMG);
+colormap(gray);
+colorbar;
+pause;
