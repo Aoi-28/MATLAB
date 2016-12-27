8a1c24e02c8e91c2b0aced628a3e5f020a8a3301
aoi <14ec010@ms.dendai.ac.jp>
2016-12-27 16:41:06 +0900

    課題9 プログラム

--- Program/Program9.m
+++ Program/Program9.m
@@ -9,11 +9,53 @@ colormap(gray);
 colorbar;
 pause;
 
+
+h = fspecial('average', 3);%平滑化フィルタ定義
+f=[0,-1,0;-1,5,-1;0,-1,0];%フィルタ設計
+
 %ノイズをのせる
-IMG = imnoise(IMG, 'poisson');
-imagesc(IMG);
+poi = imnoise(IMG, 'poisson');
+imagesc(poi);
 colormap(gray);
 colorbar;
 pause;
 
+%平滑化フィルタの適用
+Y = filter2(h,poi);
+imagesc(Y);
+colormap(gray);
+colorbar;
+pause;
 
+%メディアンフィルタの適用
+poi = medfilt2(poi, [3 3]);
+imagesc(poi);
+colormap(gray);
+colorbar;
+pause;
+
+%別ノイズによる確認
+salt = imnoise(IMG, 'salt & pepper', 0.02);
+imagesc(salt);
+colormap(gray);
+colorbar;
+pause;
+
+Y2 = filter2(h,salt);
+imagesc(Y2);
+colormap(gray);
+colorbar;
+pause;
+
+salt = medfilt2(salt, [3 3]);
+imagesc(salt);
+colormap(gray);
+colorbar;
+pause;
+
+%鮮鋭化フィルタ適用
+IMG = filter2(f, IMG, 'same');
+imagesc(IMG);
+colormap(gray);
+colorbar;
+pause;
